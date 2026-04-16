e786a877a664ac06746da9a614f9b43d8e7aa455
kyle <kylegutierrez51@gmail.com>
2026-04-16 00:25:46 -0400

    Initial Commit

---
+++ js/cloneArray.test.js
@@ -0,0 +1,8 @@
+const cloneArray = require('./cloneArray');
+
+test('properly clones array', () => {
+  const array = [1, 2, 3]
+  // expect(cloneArray(array)).toBe(array); -- this doesn't work since the cloned array points to different memory
+  expect(cloneArray(array)).toEqual(array);
+  expect(cloneArray(array)).not.toBe(array);
+})
